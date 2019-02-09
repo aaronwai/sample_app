73d4116dc6695a1aa46640a319971c5871196515
lung wai <aaron.lung.wai@gmail.com>
2019-02-09 17:48:02 +0800

    new files added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  # root 'welcome/home'  set the root as welocme controller home action
+  # get will route the path
+  # check the path by 'rake routes'
+  get 'welcome/home' , to: 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
+
 end
